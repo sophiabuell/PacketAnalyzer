855d904ce9a3c39558988c52004cfaa113c0cc31
Sophie Buell <sbuell@apple.com>
2020-10-12 04:27:24 -0700

    adding readme content and comments and some code cleanup

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # PacketAnalyzer
+
+usage: alarm.py [-h] [-i INTERFACE] [-r PCAPFILE]
+
+A network sniffer that identifies basic vulnerabilities
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -i INTERFACE  Network interface to sniff on
+  -r PCAPFILE   A PCAP file to read
+
+##Correctly Implemented: 
+To the best of my knowledge all elements of the project are completed correctly. 
+I had some trouble with the protocols and making the analizer work correctly for all of 
+them. VLAN was one I was unable to get to work correctly because it has no IP layer.
+
+##Collaboration: 
+I talked with Viet about the assignment without discussing specifics or sharing any code.
+I also asked general questions of my community and friends in the cyber security world 
+ on VetSec and OperationCode CyberSec slack channels for clarification on how attacks/scans 
+ worked and some lingering questions I had after the lectures. My brother also had fun 
+ answering some of my lingering questions on how these scans are implemented. 
+
+## Hours: 
+I spent approx 8 hours total on the assignment including reading and research. Though I spent a 
+bit of extra time coming back up to speed with python combining packets to reasonable clusters
+to reduce number of alerts. I removed that portion of the code as its not effective for a real
+time analyzer. 
+
+##Dependencies
+I used python 3, 
+scapy, argparse. 
+
+## 
+
+## What I would do differently: 
+I would group the packets by scan reasonably to not overload the user with thousands of alerts if 
+they are analyzing a static pcap. With regard to detecting them I would want to work on expanding 
+the protocols my sniffer detects. 
--- alarm.py
+++ alarm.py
@@ -25,27 +25,37 @@ def tryForAuth(packet):
     global user_ips
     global user_names
     req = packet.getlayer('HTTP Request')
+
+    #Check for Basic Authorization
     if req:
         auth = req.Authorization
         if auth:
             if auth.startswith(b'Basic '):
                 uname, passw = base64_bytes(auth.split(None, 1)[1]).split(b':', 1)
                 printUserPass(packet, uname.decode().strip(), passw.decode().strip())
+
+    #Otherwise check for IMAP and FTP Authorization
     else:
         try:
             load = packet.load
         except:
             return
+
+        #Discover username in ftp packet and store it for pairing
         if load.find(b'USER ') >= 0:
             uname = load.split(None, 1)[1].decode('ascii').strip()
             user_ips.append(packet[IP].src)
             user_names.append(uname)
+
+        #Discover password in ftp packet and retrieve username
         elif load.find(b'PASS ') >=0:
             pswd = load.split(None, 1)[1].decode('ascii').strip()
             uname = user_names[user_ips.index(packet[IP].src)]
             user_ips.remove(packet[IP].src)
             user_names.remove(uname)
             printUserPass(packet, uname, pswd)
+
+        #Checking for IMAP Auth
         elif load.find(b'LOGIN ') >= 0:
             uname = load.split(None, 1)[1].decode('ascii')
             pswd = load.split(None, 1)[1].decode('ascii')
@@ -57,6 +67,10 @@ def checkForFin(packet, otherScanDetected):
     global counter
     protocol = packet[IP].summary().split('/')[1].split(' ')[1]
     flags = packet[protocol].flags
+
+    #Check for ONLY Fin flag
+    #TODO: I should check not just that F is there without A but that there are no other flags.
+    # Though because of the check for other scans we know that at least P or U is not set
     if flags.F and not otherScanDetected and not flags.A:
         printAlert("FIN", packet, None)
         return True
@@ -66,12 +80,9 @@ def checkForFin(packet, otherScanDetected):
 #Check for xmas first because this could be caught in FIN because that flag is also set.
 def checkForXmas(packet, protocol, otherScanDetected):
     global counter
-    try:
-        flags = packet[protocol].flags
-    except:
-        print(packet[IP].summary())
-        print(protocol)
-        packet.show()
+    flags = packet[protocol].flags
+
+    #Check that flags FPU are set together
     if flags.F and flags.P and flags.U and not otherScanDetected:
         printAlert("XMAS", packet, None)
         return True
@@ -83,26 +94,38 @@ def checkForNikto(packet):
     try:
         raw = packet.load.decode().strip()
     except:
+        #If there is no load then we can say there is probably not a Nikto
         return False
+
+    #Check for multiple cases of the word.
     if 'nikto' in raw or 'Nikto' in raw:
         printAlert("NIKTO", packet, raw)
         return True
+
     return False
 
 def checkForNull(packet, protocol):
     flags = packet[protocol].flags
+
+    #Make sure flags is None type
     if not flags:
         printAlert("NULL", packet, None)
         return True
+
     return False
 
 def checkForSMB(packet):
     global counter
+
+    #If there's another scan then this probably isn't the scan thats happening
     if not scanDetected:
         port = packet.dport
+
+        #Check the port that is being connected to. These are the SMB ports.
         if port == 139 or port == 445:
             printAlert("SMB", packet, None)
             return True
+
     return False
 
 def identifyAndPrintAlerts(packet):
@@ -110,6 +133,7 @@ def identifyAndPrintAlerts(packet):
     try:
         protocol = packet[IP].summary().split('/')[1].split(' ')[1]
     except:
+        # We should always check for Nikto
         return checkForNikto(packet)
 
     if 'TCP' in protocol:
@@ -118,17 +142,24 @@ def identifyAndPrintAlerts(packet):
         checkForNull(packet, protocol)
         checkForSMB(packet)
 
+    # We should always check for Nikto
     scanDetected = checkForNikto(packet)
+
     return scanDetected
 
 def packetcallback(packet):
-     try:
-        tryForAuth(packet)
-        identifyAndPrintAlerts(packet)
-     except:
-         print("Exception occurred when trying to read packet: ")
-         packet.show()
-         print("Please check. ")
+
+    try:
+       tryForAuth(packet)
+       identifyAndPrintAlerts(packet)
+
+    #Handle exceptions thrown by declaring that an exception occured and printing the packet so the
+    # user/developer can inspect it. Ideally we would send it to a log file for analysis. We would
+    # actually do that with all alerts.
+    except:
+        print("Exception occurred when trying to read packet: ")
+        packet.show()
+        print("Please check. ")
 
 
 parser = argparse.ArgumentParser(description='A network sniffer that identifies basic vulnerabilities')
